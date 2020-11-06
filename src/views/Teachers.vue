1bc045ee52619ab564da47be7100b4c49b899c7b
MichelFonseca1024 <michelfonseca.nic123@gmail.com>
2020-11-06 14:32:36 -0300

    alter in labels

--- src/views/Teachers.vue
+++ src/views/Teachers.vue
@@ -14,7 +14,7 @@
         header-row-class-name="thead-light"
         :data="projects"
       >
-        <el-table-column label="Project" min-width="310px" prop="name">
+        <el-table-column label="Professor" min-width="310px" prop="name">
           <template v-slot="{ row }">
             <b-media no-body class="align-items-center">
               <a href="#" class="avatar rounded-circle mr-3">
@@ -28,10 +28,10 @@
             </b-media>
           </template>
         </el-table-column>
-        <el-table-column label="Budget" prop="budget" min-width="140px">
+        <el-table-column label="Area de Atuação" prop="budget" min-width="160px">
         </el-table-column>
 
-        <el-table-column label="Status" min-width="170px" prop="status">
+        <el-table-column label="Turmas" min-width="170px" prop="status">
           <template v-slot="{ row }">
             <badge class="badge-dot mr-4" type="">
               <i :class="`bg-${row.statusType}`"></i>
@@ -42,7 +42,7 @@
           </template>
         </el-table-column>
 
-        <el-table-column label="Users" min-width="190px">
+        <el-table-column label="Disciplinas" min-width="140px">
           <div class="avatar-group">
             <a
               href="#"
@@ -52,34 +52,11 @@
             >
               <img alt="Image placeholder" src="img/theme/team-1.jpg" />
             </a>
-            <a
-              href="#"
-              class="avatar avatar-sm rounded-circle"
-              data-toggle="tooltip"
-              data-original-title="Romina Hadid"
-            >
-              <img alt="Image placeholder" src="img/theme/team-2.jpg" />
-            </a>
-            <a
-              href="#"
-              class="avatar avatar-sm rounded-circle"
-              data-toggle="tooltip"
-              data-original-title="Alexander Smith"
-            >
-              <img alt="Image placeholder" src="img/theme/team-3.jpg" />
-            </a>
-            <a
-              href="#"
-              class="avatar avatar-sm rounded-circle"
-              data-toggle="tooltip"
-              data-original-title="Jessica Doe"
-            >
-              <img alt="Image placeholder" src="img/theme/team-4.jpg" />
-            </a>
+           
           </div>
         </el-table-column>
 
-        <el-table-column label="Completion" prop="completion" min-width="240px">
+        <el-table-column label="Ações" prop="completion" min-width="140px">
           <template v-slot="{ row }">
             <div class="d-flex align-items-center">
               <span class="completion mr-2">{{ row.completion }}%</span>
