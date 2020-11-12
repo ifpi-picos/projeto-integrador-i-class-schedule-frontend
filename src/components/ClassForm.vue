d2022a026f74c559204e3eee6de00d8f8a5f1dd1
AckermanFernando <fernandoloiola03@gmail.com>
2020-11-12 17:40:15 -0300

    Adicionando campos ao formulario de cadastro das classes

---
+++ src/components/ClassForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <b-modal id="addclass" title="Título do modal" size="lg">
+    <b-form ref="form" @submit.stop.prevent="handleSubmit">
+      <h6 class="heading-small text-muted mb-4">Cadastro das turmas</h6>
+
+      <div class="pl-lg-4">
+        <b-row>
+          <b-col lg="6">
+            <base-input
+              type="text"
+              label="Nome da turma"
+              placeholder="INFO III"
+              v-model="clas.username"
+              error="teste"
+              rules="required"
+              required
+            >
+            </base-input>
+          </b-col>
+
+          <b-col lg="6">
+            <base-input
+              type="text"
+              label="Curso"
+              placeholder="Informática"
+              v-model="clas.course"
+              name="Curso"
+              required
+            >
+            </base-input>
+          </b-col>
+        </b-row>
+
+        <b-row>
+          <b-col lg="6">
+            <base-input label="Local da aula">
+              <select v-model="clas.location" class="form-control">
+                <option>Local 1</option>
+                <option>Local 2</option>
+                <option>Local 3</option>
+                <option>Local 4</option>
+              </select>
+            </base-input>
+          </b-col>
+        </b-row>
+      </div>
+    </b-form>
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: "ClassForm",
+  data() {
+    return {
+      clas: {
+        username: "",
+      },
+    };
+  },
+};
+</script>
--- src/views/Class.vue
+++ src/views/Class.vue
@@ -3,6 +3,24 @@
     <base-header
       class="pb-6 pb-8 pt-5 pt-md-8 bg-gradient-success"
     ></base-header>
+    <ClassForm idModal="addclass" />
+    <b-container fluid class="mt--7">
+      <b-card>
+        <b-card-header class="pt-0 pl-0 pr-0">
+          <b-row align-h="between">
+            <b-col>
+              <h3>Turmas</h3>
+            </b-col>
+            <b-col class="text-right">
+              <b-button variant="success" v-b-modal.addclass>
+                <i class="ni ni-fat-add"></i>
+                adicionar
+              </b-button>
+            </b-col>
+          </b-row>
+        </b-card-header>
+      </b-card>
+    </b-container>
   </div>
 </template>
 
