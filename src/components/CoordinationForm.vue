b68380690ad29d4f6d12c275c2e65cbf377e2efd
MichelFonseca1024 <michelfonseca.nic123@gmail.com>
2021-08-24 22:35:18 -0300

    fix: form

--- src/components/CoordinationForm.vue
+++ src/components/CoordinationForm.vue
@@ -17,11 +17,9 @@
             </base-input>
           </b-col>
         </b-row>
-        <b-row>
+        <!-- <b-row>
           <b-col lg="11">
             <div class="form-group">
-              <!-- {{ teacher }} <br />
-            {{ registry.idResponsible }} <br /> -->
               <AutoComplete
                 v-model="teacher"
                 :options="teachers"
@@ -34,45 +32,33 @@
               />
             </div>
           </b-col>
-        </b-row>
+        </b-row> -->
 
-        <!-- <b-row>
+        <b-row>
           <b-col lg="11">
+            <label class="form-control-label">
+              Responsável
+              <span class="text-red">*</span>
+            </label>
+
             <b-form-select v-model="registry.idResponsible" class="mb-3">
               <b-form-select-option
                 v-for="teacher in teachers"
                 :key="teacher.id"
                 :value="teacher.id"
-                >{{ teacher.name }}</b-form-select-option
               >
+                {{ teacher.name }}
+              </b-form-select-option>
             </b-form-select>
-            <div class="mt-3">
-              Selected: <strong>{{ registry.idResponsible }}</strong>
-            </div>
-          </b-col>
-        </b-row> -->
-
-        <!--<b-row>
-          <b-col lg="11">
-            <base-input
-              type="text"
-              label="Nome do responsável"
-              placeholder="Nome do responsável"
-              name="Nome do responsável"
-              v-model="registry.idResponsible"
-              required
-            >
-            </base-input>
           </b-col>
         </b-row>
-        -->
 
         <b-row>
           <b-col lg="11">
             <base-input
               type="email"
-              label="email do responsável"
-              placeholder="coordenacao.edu@gmail.com"
+              label="Email da coordenação"
+              placeholder="coordenacao@ifpi.edu.br"
               name="email"
               v-model="registry.email"
               required
@@ -83,14 +69,13 @@
       </div>
     </b-form>
     <template #modal-footer="{ hide }">
-      <!-- Emulate built in modal footer ok and cancel button actions -->
       <b-button variant="outline-danger" @click="hide('forget')">
         Cancelar
       </b-button>
       <b-button
         :disabled="!checkFormValidity()"
         variant="success"
-        @click="handleSubmit()"
+        @click="handleOk('coordinations')"
       >
         Salvar
       </b-button>
@@ -100,20 +85,11 @@
 
 <script>
 import modalForm from '../mixins/modalForm'
-import AutoComplete from './AutoComplete.vue'
 
 export default {
   name: 'CoordinationForm',
   mixins: [modalForm],
-  components: {
-    AutoComplete
-  },
-  data () {
-    return {
-      teachers: [],
-      teacher: ''
-    }
-  },
+
   props: {
     idModal: {
       type: String,
@@ -131,28 +107,19 @@ export default {
     }
   },
 
-  methods: {
-    handleSubmit () {
-      console.log(this.registry)
-      this.handleOk('coordinations')
-    },
-    async searchTeachers (query) {
-      try {
-        const { data } = await this.$axios.get(
-          `https://empty-coffee-cups.herokuapp.com/api/teachers/search?name=${query}`
-        )
-        this.teachers = data.data
-        // if (query == '') {
-        //   this.teachers = []
-        // }
-      } catch (err) {
-        console.log(err)
-      }
-    },
-    onSelect (teacher) {
-      this.registry.idResponsible = teacher.id
-      // this.registry.id = teacher.id
-      // console.log(teacher.id)
+  data () {
+    return {
+      teachers: [],
+      teacher: ''
+    }
+  },
+
+  async created () {
+    try {
+      const { data } = await this.$axios.get('/teachers/available-coordinators')
+      this.teachers = data.data
+    } catch (err) {
+      console.log(err)
     }
   }
 }
